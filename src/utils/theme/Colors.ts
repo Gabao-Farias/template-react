b79796032723c144e007b94819dc243cfdf399af
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-03-09 00:32:23 -0300

    fixed template eslint errors

--- src/utils/theme/Colors.ts
+++ src/utils/theme/Colors.ts
@@ -9,22 +9,22 @@ export const Colors: DarkLightTheme = {
       dark: '#141414',
       main: '#e7ebe6',
       light: '#eef2ed',
-      lighter: '#f5f5f5'
+      lighter: '#f5f5f5',
     },
     primary: {
       darker: '#094f12',
       dark: '#0a7017',
       main: '#10b324',
       light: '#35f04e',
-      lighter: '#69ff7c'
+      lighter: '#69ff7c',
     },
     environment: {
       darker: '#111211',
       dark: '#222422',
       main: '#303330',
       light: '#414541',
-      lighter: '#5b615b'
-    }
+      lighter: '#5b615b',
+    },
   },
   light: {
     error: '#ed4747',
@@ -36,23 +36,23 @@ export const Colors: DarkLightTheme = {
       dark: '#141414',
       main: '#212121',
       light: '#eef2ed',
-      lighter: '#f5f5f5'
+      lighter: '#f5f5f5',
     },
     primary: {
       darker: '#094f12',
       dark: '#0a7017',
       main: '#10b324',
       light: '#35f04e',
-      lighter: '#69ff7c'
+      lighter: '#69ff7c',
     },
     environment: {
       darker: '#b7c7b7',
       dark: '#cad9ca',
       main: '#d5e3d5',
       light: '#e6f5e6',
-      lighter: '#f0fff0'
-    }
-  }
+      lighter: '#f0fff0',
+    },
+  },
 };
 
 export default Colors;
