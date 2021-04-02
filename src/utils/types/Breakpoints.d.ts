cff35cdd6f693ca9cb82e19e11aa4e8069cfc7a6
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-04-01 23:47:33 -0300

    added new theming schemas

---
+++ src/utils/types/Breakpoints.d.ts
@@ -0,0 +1,9 @@
+declare type Breakpoints = {
+  smallMobile: number;
+  mobileMedium: number;
+  mobileWider: number;
+  tablet: number;
+  smallLaptop: number;
+  fullHD: number;
+  ultraWide: number;
+};
---
+++ src/utils/types/Radius.d.ts
@@ -0,0 +1,12 @@
+declare type Radius = {
+  insane: number;
+  huge: number;
+  giant: number;
+  big: number;
+  large: number;
+  medium: number;
+  short: number;
+  small: number;
+  smaller: number;
+  meaningless: number;
+};
